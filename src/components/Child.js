26c02c40a1a859e17fa2f3c32c16a66eb8004a82
quangle <quangle.bxl@gmail.com>
2019-12-09 18:08:39 +0100

    FEATURE: call useEffect to modify stream values inside BLoC

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,10 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import useStream from "./useStream";
 import BlocContext from "../contexts/BlocContext";
 
 const Child = ({ snapshot }) => {
   const context = useContext(BlocContext);
   const localStream = useStream(context.testValue.stream());
+
+  // Modifying stream values inside the BLoC from here
+  // creates an infinite loop.
+  //context.testValue.inStream("from Child");
+  //context.testValue.value = "child value";
+  // use useEffect, like so (fill in the array as needed):
+  useEffect(() => {
+    context.testValue.inStream("from Child");
+    context.testValue.value = "child value";
+  }, []);
+
   return (
     <div>
       <div>From useStream hook : {snapshot}</div>
